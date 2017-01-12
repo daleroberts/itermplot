20bf6116f72b51153bf4379937c3ef4463cbf6ea
Dale Roberts <dale.o.roberts@gmail.com>
2017-01-12 15:56:49 +1100

    Nicer example

--- test.py
+++ test.py
@@ -1,9 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import networkx as nx
 
 plt.rcParams["font.size"] = 10
 
-plt.figure(figsize=(5,3))
-plt.plot(np.sin(np.sinc(np.arange(0,10,0.001))), 'r', lw=2)
+plt.figure(figsize=(8,3))
+
+ax = plt.subplot(121)
+x = np.arange(0,10,0.001)
+ax.plot(x, np.sin(np.sinc(x)), 'r', lw=2)
+
+ax = plt.subplot(122)
+G = nx.random_geometric_graph(200,0.125)
+pos=nx.spring_layout(G)
+nx.draw_networkx_edges(G, pos, alpha=0.2)
+nx.draw_networkx_nodes(G, pos, node_color='r', node_size=10, edge_color='g')
 
 plt.show()
