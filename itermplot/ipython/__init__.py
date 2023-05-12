966868cdd9a1d32df3c48858c24e01b9eb5fe7bd
Zane Dufour <zane@znd4.me>
2023-05-12 14:27:28 -0400

    add ipython extension

---
+++ itermplot/ipython/__init__.py
@@ -0,0 +1,23 @@
+"""
+ipython extensions for image output handling.
+e.g.
+
+```ipython
+%load_ext itermplot.ipython
+```
+
+implemented based on https://ipython.readthedocs.io/en/stable/config/shell_mimerenderer.html
+"""
+
+from . import imgcat
+
+
+def register_mimerenderer(ipython, mime, handler):
+    ipython.display_formatter.active_types.append(mime)
+    ipython.display_formatter.formatters[mime].enabled = True
+    ipython.mime_renderers[mime] = handler
+
+
+def load_ipython_extension(ipython):
+    register_mimerenderer(ipython, "image/png", imgcat)
+    register_mimerenderer(ipython, "image/jpeg", imgcat)
